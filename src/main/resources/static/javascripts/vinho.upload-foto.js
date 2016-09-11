b3bfe9514ea9921c0ffd30641a807ea35be3e933
Julio Zittei <julio.zittei@hotmail.com>
2016-09-11 00:07:31 -0300

    Finalização do projeto

--- src/main/resources/static/javascripts/vinho.upload-foto.js
+++ src/main/resources/static/javascripts/vinho.upload-foto.js
@@ -1,20 +1,46 @@
+var Wine = Wine || {};
 
-$(function(){
-	var uploadDrop = $('#upload-drop');
-	var containerFoto = $('.js-container-foto');
+Wine.UploadFoto = (function(){
 	
+	function UploadFoto(){
+		this.uploadDrop = $('#upload-drop');
+		this.containerFoto = $('.js-container-foto');
+	}
+	
+	UploadFoto.prototype.iniciar = function(){
 		var settings = {
 			type: 'json',
 			filelimit: 1,
 			allow: '*.(jpg|jpeg|png)',
 			action: '/fotos/' + uploadDrop.data('codigo'),
-			complete: function(foto){
-				uploadDrop.addClass('hidden');
-				containerFoto.prepend('<img src="'+foto.url+'" class="img-responsive" style="margin:auto;"/>');
-			}
+			complete: onUploadComplete.bind(this),			
+			beforeSend: adicionarCsfrToken
 		};
+			
+			UIkit.uploadSelect($('#upload-select'), settings);
+			UIkit.uploadDrop(this.uploadDrop, settings);	
+	}
+	
+	function adicionarCsrfToken(xhr){
+		var header = $('input[name =_csrf_header]').val();
+		var token = $('input[name = _csrf').val();
+		xhr.setRequestHeader(header, token);
+	}
+	
+	function onUploadComplete(){
+		this.uploadDrop.addClass('hidden');
+		this.containerFoto.prepend('<img src="'+foto.url+'" class="img-responsive" style="margin:auto;"/>');
+	}
+	
+	return UploadFoto;
+	
+})();
+
+$(function(){
+	
+	var uploadFoto = new Wine.UploadFoto();
+	uploadFoto.iniciar();
+	
 		
-		UIkit.uploadSelect($('#upload-select'), settings);
-		UIkit.uploadDrop(uploadDrop, settings);	
 });
 	
