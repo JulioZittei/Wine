0a5bcbde5da33aa3e6362664988a4613567daf6f
Julio Zittei <julio.zittei@hotmail.com>
2016-09-15 17:08:07 -0300

    Atualização das configurações

--- src/main/java/br/com/wine/WineApplication.java
+++ src/main/java/br/com/wine/WineApplication.java
@@ -1,12 +1,22 @@
 package br.com.wine;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class WineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineApplication.class, args);
+	}
+	
+	@Bean
+	public FixedLocaleResolver localeResolver(){
+		return new FixedLocaleResolver(new Locale("pt","BR"));
 	}
 }
--- src/test/java/br/com/wine/WineApplicationTests.java
+++ src/test/java/br/com/wine/WineApplicationTests.java
@@ -1,4 +1,4 @@
-package com.algaworks.wine;
+package br.com.wine;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
